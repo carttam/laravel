66c27a730c030ca3b050981299e97d07b105dfc4
nop <dudu.arttam@gmail.com>
2021-09-06 19:00:33 +0430

    second init

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function (){
+   Route::get('/',[\App\Http\Controllers\AdminController::class,'index']);
 });
